f34178b17ecac4ea94322bb4fa72bed38f518785
Surenash <stiflersourena@gmail.com>
2026-04-23 12:28:36 +0530

    cleanup: remove FileViewerModal from layout in favor of Smart View page

--- manfacquot/layouts/MainLayout.tsx
+++ manfacquot/layouts/MainLayout.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { BackgroundAnimation, Header, CurrencyRatesWarning, Footer, FileViewerModal } from '../components';
-import { useFileViewer } from '../contexts/FileViewerContext';
+import { BackgroundAnimation, Header, CurrencyRatesWarning, Footer } from '../components';
 import { styles, bg_deep_space } from '../types/theme';
 
 export const MainLayout = () => {
-    const { fileViewerState, closeViewer } = useFileViewer();
-
     return (
         <div style={{ backgroundColor: bg_deep_space }}>
             <BackgroundAnimation />
@@ -16,7 +13,6 @@ export const MainLayout = () => {
                 <main style={styles.mainContent}>
                     <Outlet />
                 </main>
-                {fileViewerState.isOpen && <FileViewerModal design={fileViewerState.design} onClose={closeViewer} />}
                 <Footer />
             </div>
         </div>
